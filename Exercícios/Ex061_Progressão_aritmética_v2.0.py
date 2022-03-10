47f60607835a5293d5aa3ea66bfa513cebae12be
jefersonmatsui <jefersonmatsui@gmail.com>
2022-03-10 20:26:58 -0300

    Novo

---
+++ Exercícios/Ex061_Progressão_aritmética_v2.0.py
@@ -0,0 +1,14 @@
+# Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA,
+# mostrando os 10 primeiros termos da progressão usando a estrutura while.
+
+
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da P.A.: '))
+termo = primeiro
+count = 1
+while count <= 10:
+    print(f'{termo} - ', end='')
+    termo += razao
+    count += 1
+print('FIM')
