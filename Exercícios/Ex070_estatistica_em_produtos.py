9a8694c64888551950d187df830a19cd5c38077e
jefersonmatsui <jefersonmatsui@gmail.com>
2022-03-24 17:30:50 -0300

    Novo

---
+++ Exercícios/Ex070_estatistica_em_produtos.py
@@ -0,0 +1,26 @@
+# Crie um programa que leia o nome e o preço de vários produtos.
+# O programa deverá perguntar se o usuário vai continuar ou não. No final, mostre:
+# A) qual é o total gasto na compra.
+# B) quantos produtos custam mais de R$1000.
+# C) qual é o nome do produto mais barato.
+
+total = totalmil = count = menor = 0
+barato = ' '
+while True:
+    produto = str(input('Nome do Produto: '))
+    preco = float(input('Preço: R$'))
+    total += preco
+    if preco > 1000:
+        totalmil += 1
+    if count == 1 or preco < menor:
+        menor = preco
+        barato = produto
+
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar [S/N]: ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'O total da compra foi de R${total:.2f}')
+print(f'Temos {totalmil} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
